d5db150b04f4927553be10a78cdedc2994b2c773
e <995549467@qq.com>
2018-04-03 11:31:59 +0800

    配置文件

---
+++ spring-cloud-config/src/main/java/com/lh/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.lh.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: LH
+ * @Description:
+ * @Date: 2018/4/3 0003 10:08
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
