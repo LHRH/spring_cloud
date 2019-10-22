fc38771834a0498882cbb6e3e272187bf7a35466
令狐 <995549467@qq.com>
2019-10-22 20:15:31 +0800

    提交测试

--- spring_consumer/src/main/java/com/lh/cloud/controller/UserController.java
+++ spring_consumer/src/main/java/com/lh/cloud/controller/UserController.java
@@ -22,6 +22,7 @@ public class UserController {
     @ResponseBody
     public String getUser() {
         RestTemplate template = getRestTemplate();
+        System.out.println("提交测试");
         return template.getForObject("http://spring-provider-service/get/110", String.class);
     }
 }
